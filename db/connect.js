c4a9f9996b4a0a9aca0f4b580f9e1364d00a24db
tr15tan01 <t.varamashvili@gmail.com>
2023-09-22 23:49:26 +0400

    remo

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,4 @@
 require("dotenv").config();
-// console.log(process.env); // remove thi
-// const connectionString = process.env.CONNECTION;
-//   "mongodb+srv://TaskManager:l9XwQkTX9lBjcMBP@cluster0.tpkz7.mongodb.net/TaskManagerSm?retryWrites=true&w=majority";
-
-// console.log(connectionString);
 
 const mongoose = require("mongoose");
 
